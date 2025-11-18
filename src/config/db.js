16eb13519d28ffd1356358ed9aa7ae3746adb203
srinibasnayak23 <srinibasnayak23@gmail.com>
2025-11-18 13:03:09 +0530

    mongo url updated

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     //await mongoose.connect(process.env.MONGO_URI);
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect("mongodb+srv://devsrinibas_db_user:Ge85sJZgjBBQQuwA@freecluster.ju5itlx.mongodb.net/?appName=FreeCluster");
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB Error:", err);
